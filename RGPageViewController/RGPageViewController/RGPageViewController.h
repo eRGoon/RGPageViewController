61968fe5e53a6fbc43fb028edfc2c7fceb69dd94
Ronny Gerasch <rg@ergoon.com>
2017-01-23 10:07:11 +0700

    creating framework target
    Signed-off-by: Ronny Gerasch <rg@ergoon.com>

---
+++ RGPageViewController/RGPageViewController/RGPageViewController.h
@@ -0,0 +1,19 @@
+//
+//  RGPageViewController.h
+//  RGPageViewController
+//
+//  Created by Ronny Gerasch on 23.01.17.
+//  Copyright © 2017 Ronny Gerasch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RGPageViewController.
+FOUNDATION_EXPORT double RGPageViewControllerVersionNumber;
+
+//! Project version string for RGPageViewController.
+FOUNDATION_EXPORT const unsigned char RGPageViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RGPageViewController/PublicHeader.h>
+
+
